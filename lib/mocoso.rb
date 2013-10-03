c5a9d54690f1a2f30da0437abb5b5f26ad6e4cc5
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-10-02 20:15:02 -0500

    Add docs to Mocoso#mock

--- lib/mocoso.rb
+++ lib/mocoso.rb
@@ -35,6 +35,39 @@ module Mocoso
   end
   private :stub_method_name
 
+  # Expect that method `method` is called with `:with` option and
+  # return `:return` option. If expectations are not met, it raises
+  # `Mocoso::ExpectationError` error.
+  #
+  #   class User < Model
+  #   end
+  #
+  #   user = User[1]
+  #
+  #   Mocoso.expect user, :update, with: [{ name: 'new name' }], returns: true
+  #
+  #   subject.update unexpected: nil
+  #   # => Mocoso::ExpectationError: Expected [{:name=>"new name"}], got [{:unexpected=>nil}]
+  #
+  #   user.update name: 'new name'
+  #   # => true
+  #
+  # Note that it will rewrite the method in the object. If you want to set an
+  # expectation without side effects, you must pass a block:
+  #
+  #   class User < Ohm::Model
+  #   end
+  #
+  #   User.exists? 1
+  #   # => false
+  #
+  #   Mocoso.expect User, :exists?, with: [1], returns: true do
+  #     User.exists? 1
+  #     # => true
+  #   end
+  #
+  #   User.exists? 1
+  #   # => false
   def expect object, method, options
     expectation = -> *params {
       with = options.fetch(:with) { [] }
